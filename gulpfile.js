1ee75a4fdedf1e56fa5ed2fe97c9d97b4dedd068
MatheusH <matheushds.br@gmail.com>
2020-03-18 23:23:56 -0300

    correct destination path for style files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
 //compile scss into css
 function style() {
-  return gulp.src('app/src/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('app/css'))
+  return gulp
+    .src("app/src/style.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("app/src/css"))
     .pipe(browserSync.stream());
 }
 function watch() {
@@ -15,9 +16,9 @@ function watch() {
       index: "/index.html"
     }
   });
-  gulp.watch('app/src/**/*.scss', style)
-  gulp.watch('./app/*.html').on('change', browserSync.reload);
-  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+  gulp.watch("app/src/**/*.scss", style);
+  gulp.watch("./app/*.html").on("change", browserSync.reload);
+  gulp.watch("./js/**/*.js").on("change", browserSync.reload);
 }
 exports.style = style;
 exports.watch = watch;
